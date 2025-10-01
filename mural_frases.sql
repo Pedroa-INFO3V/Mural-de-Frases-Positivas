6f6c3be0652af1feb7e2676b385b8174ec596f75
Pedro Henrique <p.xavier@escolar.ifrn.edu.br>
2025-09-30 23:46:06 -0300

    banco

---
+++ mural_frases.sql
@@ -0,0 +1,131 @@
+CREATE DATABASE  IF NOT EXISTS `mural_frases` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `mural_frases`;
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mural_frases
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `curtidas`
+--
+
+DROP TABLE IF EXISTS `curtidas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `curtidas` (
+  `usuario_id` int NOT NULL,
+  `frase_id` int NOT NULL,
+  `quantidade` int DEFAULT NULL,
+  PRIMARY KEY (`usuario_id`,`frase_id`),
+  KEY `fk_curtida_frase_idx` (`frase_id`),
+  CONSTRAINT `fk_curtida_frase` FOREIGN KEY (`frase_id`) REFERENCES `frases` (`id`),
+  CONSTRAINT `fk_curtida_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `curtidas`
+--
+
+LOCK TABLES `curtidas` WRITE;
+/*!40000 ALTER TABLE `curtidas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `curtidas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `frases`
+--
+
+DROP TABLE IF EXISTS `frases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `frases` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario_id` int NOT NULL,
+  `conteudo` varchar(45) NOT NULL,
+  `data_postagem` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_frase_usuario_idx` (`usuario_id`),
+  CONSTRAINT `fk_frase_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `frases`
+--
+
+LOCK TABLES `frases` WRITE;
+/*!40000 ALTER TABLE `frases` DISABLE KEYS */;
+/*!40000 ALTER TABLE `frases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `negocio`
+--
+
+DROP TABLE IF EXISTS `negocio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `negocio` (
+  `aejvnn3we` int NOT NULL,
+  `enkww` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`aejvnn3we`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `negocio`
+--
+
+LOCK TABLES `negocio` WRITE;
+/*!40000 ALTER TABLE `negocio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `negocio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome_usuario` varchar(50) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nome_usuario_UNIQUE` (`nome_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-30 23:41:02
